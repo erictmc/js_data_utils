7e141a14c6b0fb0ddc905141d8f194b1fec5aac4
erictmc <etmcmillen@gmail.com>
2018-02-08 00:29:43 -0800

    Adding time utilities

---
+++ time.js
@@ -0,0 +1,63 @@
+import {isEmptyValue} from "./index";
+
+import moment from "moment-timezone";
+
+
+/*
+ * In order to handle testing with jest, see:
+ * https://github.com/facebook/jest/issues/2123
+ */
+export const InvalidTimeError = () => {};
+InvalidTimeError.prototype = Object.create(Error.prototype);
+
+/*
+ * Determines if two ISO 8601 timestamps occur on the same date
+ * @param { string } timestamp : a string timestamp
+ * @param { string } timestamp : a string timestmap
+ */
+export const occurOnSameDate = (timestamp1, timestamp2) => {
+  const ts1 = moment(timestamp1);
+  const tsMonth1 = ts1.month() + 1;
+  const tsDay1 = ts1.date();
+  const tsYear1 = ts1.year();
+
+  const ts2 = moment(timestamp2);
+  const tsMonth2 = ts2.month() + 1;
+  const tsDay2 = ts2.date();
+  const tsYear2 = ts2.year();
+
+  return tsMonth1 === tsMonth2 && tsDay1 === tsDay2 && tsYear1 === tsYear2;
+};
+
+
+/*
+ * Takes string clock time and returns parsed hour and minute values
+ * @param { string } clockTime : a string of the format 'HH:mm', where
+ * hours can range from 0 - 23 and minutes can range from 1 - 59.
+ */
+export const parseClockTime = (clockTime) => {
+
+  if (isEmptyValue(clockTime)) {
+    throw new InvalidTimeError();
+  }
+
+  const timeComponents = clockTime.split(":");
+
+  if (timeComponents.length !== 2) {
+    throw new InvalidTimeError();
+  }
+
+  const hour = Number(timeComponents[0]);
+  const minute = Number(timeComponents[1]);
+
+  if (isNaN(hour) || isNaN(minute)) {
+    throw new InvalidTimeError();
+  }
+
+  if (!(hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)) {
+    throw new InvalidTimeError();
+  }
+
+  return {hour, minute}
+
+};
---
+++ time.test.js
@@ -0,0 +1,82 @@
+import {InvalidTimeError, occurOnSameDate, parseClockTime} from "./time";
+
+describe("occurOnSameDate", () => {
+
+  it(" determine if a timestamp is on a date", () => {
+    const timestamp = "2016-12-31T12:00:00";
+    const date = "2016-12-31T00:00:00";
+
+    expect(occurOnSameDate(date, timestamp)).toEqual(true);
+
+  });
+
+  it(" determine if a timestamp is NOT on a date", () => {
+    const timestamp = "2016-12-31T12:00:00";
+    const date = "2016-12-30T00:00:00";
+
+    expect(occurOnSameDate(date, timestamp)).toEqual(false);
+  });
+});
+
+describe("parseClockTime", () => {
+
+  it(" it should throw an error for invalid input", () => {
+    const genericInvalidCases = [
+      "",
+      "23:01:00"
+    ];
+
+    genericInvalidCases.forEach((x) => {
+      expect(() => {
+        parseClockTime(x)
+      }).toThrowError(InvalidTimeError);
+    });
+
+
+  });
+
+  it(" both hour and minute values should be numerical", () => {
+    const nonNumericalCases = [
+      "23:aa",
+      "aa:15",
+      "24a:15",
+      "24:15b"
+    ];
+
+    nonNumericalCases.forEach((x) => {
+      expect(() => {
+        parseClockTime(x)
+      }).toThrowError(InvalidTimeError);
+    })
+
+  });
+
+  it(" both hour and minute values should be feasible numerical values", () => {
+    const infeasibleNumericalCases = [
+      "23:-1",
+      "-1:00",
+      "24:00",
+      "23:60"
+    ];
+
+    infeasibleNumericalCases.forEach((x) => {
+      expect(() => {
+        parseClockTime(x)
+      }).toThrowError(InvalidTimeError);
+    });
+
+  });
+
+  it(" it should handle correctly formatted clock times ", () => {
+    const {hour, minute} = parseClockTime("23:00");
+
+    expect(hour).toEqual(23);
+    expect(minute).toEqual(0);
+
+    expect(() => {
+      parseClockTime("23:00")
+    }).not.toThrow(InvalidTimeError);
+
+  });
+
+});
